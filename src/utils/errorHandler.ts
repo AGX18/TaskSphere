1d84d1546f6cc55e6f9ee241102e513523c3b71d
AGX18 <agxnh2020@gmail.com>
2025-11-23 20:47:16 +0000

    better error handling

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -2,6 +2,7 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "../utils/AppError";
 import { logger } from "../utils/logger";
+import { ZodError } from "zod";
 
 export const errorHandler = (
   err: Error,
@@ -15,8 +16,16 @@ export const errorHandler = (
   let statusCode = 500;
   let message = "Internal Server Error";
 
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = "Validation Error";
+    // You can attach specific field details if you want
+    // (We will just send the first issue message for simplicity)
+    const issues = err.issues.map((e) => `${e.path.join(".")}: ${e.message}`);
+    message = `Invalid Input: ${issues.join(", ")}`;
+  }
   // If it's our custom trusted error, use its status and message
-  if (err instanceof AppError) {
+  else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
   }
